55b1cedbaeb4548934773cf5a90d07ef322b1b8f
dorhar <doron.harnoy@tensorleap.ai>
2022-05-29 15:38:59 +0300

    add sqrt operation layer

--- onnx2kerastl/operation_layers.py
+++ onnx2kerastl/operation_layers.py
@@ -6,6 +6,7 @@ from tensorflow import keras
 from tensorflow.keras import backend as K
 
 from onnx2kerastl.customonnxlayer.onnxreducemean import OnnxReduceMean
+from .customonnxlayer.onnxsqrt import OnnxSqrt
 from .utils import is_numpy, ensure_tf_type, ensure_numpy_type
 
 # Handle python 2.7 import error
@@ -231,13 +232,8 @@ def convert_sqrt(node, params, layers, lambda_func, node_name, keras_name):
 
     input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
 
-    def target_layer(x):
-        import tensorflow.keras.backend as K
-        return K.sqrt(x)
-
-    lambda_layer = keras.layers.Lambda(target_layer, name=keras_name)
-    layers[node_name] = lambda_layer(input_0)
-    lambda_func[keras_name] = target_layer
+    sqrt_layer = OnnxSqrt(name=keras_name)
+    layers[node_name] = sqrt_layer(input_0)
 
 
 def convert_split(node, params, layers, lambda_func, node_name, keras_names):
