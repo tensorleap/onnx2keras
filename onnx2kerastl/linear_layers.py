81c655662547a73903b46569c74f71ee443e042b
ranhomri <ran.homri@tensorleap.ai>
2023-05-23 10:30:42 +0300

    draft change implementing straight forward matrix multiplication in Gemm layer

--- onnx2kerastl/linear_layers.py
+++ onnx2kerastl/linear_layers.py
@@ -28,31 +28,37 @@ def convert_gemm(node, params, layers, lambda_func, node_name, keras_name):
         logger.debug('Convert GEMM without bias.')
     else:
         raise AttributeError('More than 3 or less than 2 inputs')
-
+    keras_input = layers[node.input[0]]
     # Linear can have additional flag to transpose weights
     if 'transB' in params and params['transB'] == 1:
         logger.debug('Transposing W matrix.')
         keras_weights[0] = keras_weights[0].transpose()
     # Estimate input/output neurons
     input_channels, output_channels = keras_weights[0].shape[-2:]
+    if not keras_input.shape[-1] == input_channels:
+        keras_input = tf.transpose(keras_input)
     logger.debug('Input units %s, output units %s.', input_channels, output_channels)
     if len(layers[node.input[1]].shape) > 2: #N-dim tensor multipication Dense doesn't work
         assert len(node.input) == 2
-        layers[node_name] = tf.matmul(layers[node.input[0]], layers[node.input[1]], name=keras_name)
+        layers[node_name] = tf.matmul(keras_input, layers[node.input[1]], name=keras_name)
     else:
-        if is_numpy(keras_weights[0]):
-            dense = keras.layers.Dense(
-                output_channels,
-                weights=keras_weights, name=keras_name, use_bias=has_bias
-            )
-
-            # The first input - always X
-            try:
-                layers[node_name] = dense(layers[node.input[0]])
-            except ValueError:
-                reshape = keras.layers.Reshape([input_channels], name=keras_name + '_reshape')
-                reshaped_x = reshape(layers[node.input[0]])
-                layers[node_name] = dense(reshaped_x)
+        if len(keras_weights) == 2:
+            layers[node_name] = tf.add(tf.matmul(keras_input, keras_weights[0]), keras_weights[1], name=keras_name)
+        elif len(keras_weights) == 1:
+            layers[node_name] = tf.matmul(keras_input, keras_weights[0], name=keras_name)
+        # if is_numpy(keras_weights[0]):
+        #     dense = keras.layers.Dense(
+        #         units=output_channels,
+        #         weights=keras_weights, name=keras_name, use_bias=has_bias
+        #     )
+        #
+        #     # The first input - always X
+        #     try:
+        #         layers[node_name] = dense(keras_input)
+        #     except ValueError:
+        #         reshape = keras.layers.Reshape([input_channels], name=keras_name + '_reshape')
+        #         reshaped_x = reshape(keras_input)
+        #         layers[node_name] = dense(reshaped_x)
 
         else:
-            layers[node_name] = keras.layers.Multiply()([layers[node.input[0]], layers[node.input[1]]])
+            layers[node_name] = keras.layers.Multiply()([keras_input, keras_weights[0]])
