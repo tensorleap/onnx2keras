fc890f877077edfe71c581940f56440388684b7a
tom Koren <tom.koren@tensorleap.ai>
2023-07-24 16:18:08 +0300

    add reduce min test

--- test/layers/operations/test_reduce.py
+++ test/layers/operations/test_reduce.py
@@ -14,9 +14,26 @@ class TorchReduceProd(torch.nn.Module):
         return torch.prod(x, dim=self.axis, keepdim=self.keepdim)
 
 
+class TorchReduceMin(torch.nn.Module):
+    def __init__(self, axis, keepdim):
+        super(TorchReduceMin, self).__init__()
+        self.axis = axis
+        self.keepdim = keepdim
+
+    def forward(self, x):
+        return torch.min(x, dim=self.axis, keepdim=self.keepdim)
+
+
 @pytest.mark.parametrize('axis', [-1, 1])
 @pytest.mark.parametrize('keepdim', [True, False])
 def test_reduce_prod(axis, keepdim):
     pt_model = TorchReduceProd(axis=axis, keepdim=keepdim)
+    error = convert_and_test(pt_model, (np.random.random((1, 8, 3))), verbose=False)
+
+
+@pytest.mark.parametrize('axis', [-1, 1])
+@pytest.mark.parametrize('keepdim', [True, False])
+def test_reduce_min(axis, keepdim):
+    pt_model = TorchReduceMin(axis=axis, keepdim=keepdim)
     error = convert_and_test(pt_model, (np.random.random((1, 8, 3))), verbose=False)
 
