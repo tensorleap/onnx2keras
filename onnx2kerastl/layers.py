42ff26682a3e299245ff4b65b3ca5e8d94692e41
tom Koren <tom.koren@tensorleap.ai>
2023-08-08 16:32:15 +0300

    support RNN

--- onnx2kerastl/layers.py
+++ onnx2kerastl/layers.py
@@ -2,7 +2,7 @@ from .convolution_layers import convert_conv, convert_convtranspose
 from .activation_layers import convert_relu, convert_elu, convert_lrelu, convert_selu, \
     convert_sigmoid, convert_tanh, convert_softmax, convert_prelu, convert_hard_sigmoid, convert_erf, convert_soft_plus, \
     convert_soft_sign, convert_mish, convert_gelu, convert_hard_swish
-from .ltsm_layers import convert_lstm, convert_gru
+from .ltsm_layers import convert_lstm, convert_gru, convert_rnn
 from .operation_layers import convert_clip, convert_exp, convert_neg, convert_reduce_sum, convert_reduce_mean, \
     convert_log, convert_pow, convert_sqrt, convert_split, convert_cast, convert_floor, convert_identity, \
     convert_argmax, convert_reduce_l2, convert_reduce_max, convert_reciprocal, convert_abs, convert_not, convert_cosine, \
@@ -146,5 +146,6 @@ AVAILABLE_CONVERTERS = {
     "BitShift": convert_bitshift,
     "TopK": convert_topk,
     'GatherElements': convert_gather_elements,
-    'GRU': convert_gru
+    'GRU': convert_gru,
+    'RNN': convert_rnn
 }
