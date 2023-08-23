a6932b54764f4a052dad394cc47c8fa99782c019
tom Koren <tom.koren@tensorleap.ai>
2023-08-23 09:44:30 +0300

    support unique op

--- onnx2kerastl/layers.py
+++ onnx2kerastl/layers.py
@@ -24,7 +24,7 @@ from .pooling_layers import convert_avgpool, convert_maxpool, convert_global_avg
 from .padding_layers import convert_padding
 from .upsampling_layers import convert_upsample
 from .caffe2_layers import convert_alias_with_name, convert_resize_nearest
-from .sampling_layers import convert_gridsample, convert_range
+from .sampling_layers import convert_gridsample, convert_range, convert_unique
 from .fft_layers import convert_dft
 
 AVAILABLE_CONVERTERS = {
@@ -146,5 +146,6 @@ AVAILABLE_CONVERTERS = {
     "BitShift": convert_bitshift,
     "TopK": convert_topk,
     'GatherElements': convert_gather_elements,
-    'NonMaxSuppression': convert_nms
+    'NonMaxSuppression': convert_nms,
+    'Unique': convert_unique,
 }
