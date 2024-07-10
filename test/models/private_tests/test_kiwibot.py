45ae099706394ac165bbacf2364e7786bfe58705
ranhomri <ran.homri@tensorleap.ai>
2024-07-10 13:43:59 +0300

    test for kiwibot model

---
+++ test/models/private_tests/test_kiwibot.py
@@ -0,0 +1,31 @@
+import numpy as np
+import onnx
+from onnx2kerastl import onnx_to_keras
+import tensorflow as tf
+from keras_data_format_converter import convert_channels_first_to_last
+from onnx2kerastl.customonnxlayer import onnx_custom_objects_map
+import pytest
+
+from test.models.private_tests.aws_utils import aws_s3_download
+
+@pytest.mark.parametrize('aws_s3_download', [["kiwibot/", "kiwibot/", False]], indirect=True)
+def test_mmdet_convnext(aws_s3_download):
+    onnx_model_path = f'{aws_s3_download}/model.onnx'
+    save_model_path = f'{aws_s3_download}/model.h5'
+
+    input_data = np.random.uniform(0, 255, (1, 360, 640, 3)).astype(np.uint8)
+    # load onnx model
+    onnx_model = onnx.load(onnx_model_path)
+    # extract feature names from the model
+    input_features = [inp.name for inp in onnx_model.graph.input]
+    # convert onnx model to keras
+    keras_model = onnx_to_keras(onnx_model, input_names=input_features,
+                                name_policy='attach_weights_name', allow_partial_compilation=False).converted_model
+
+    final_model = convert_channels_first_to_last(keras_model, should_transform_inputs_and_outputs=True,
+                                                 verbose=True)
+
+    final_model.save(save_model_path)
+
+    loaded_keras_model = tf.keras.models.load_model(save_model_path, custom_objects=onnx_custom_objects_map)
+    keras_output = loaded_keras_model(input_data)
