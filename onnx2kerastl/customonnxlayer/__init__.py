eab9c68bc5e92c5b10d43a9fc4f85bb70d1fa9b0
ranhomri <ran.homri@tensorleap.ai>
2024-11-03 14:44:43 +0200

    deleted custom layer

--- onnx2kerastl/customonnxlayer/__init__.py
+++ onnx2kerastl/customonnxlayer/__init__.py
@@ -1,10 +1,8 @@
-from onnx2kerastl.customonnxlayer.onnxconstantmul import ONNXMultiplyByConstantLayer
 from onnx2kerastl.customonnxlayer.onnxeinsum import OnnxEinsumLayer
 from onnx2kerastl.customonnxlayer.onnxlstm import OnnxLSTM
 
 onnx_custom_objects_map = {
     "OnnxLSTM": OnnxLSTM,
-    "ONNXMultiplyByConstantLayer": ONNXMultiplyByConstantLayer
 }
 
 onnx_custom_layers = {
