750be762212993dd493ea3207bb99f3e099e8275
nirbenzikri <nir.benzikri@tensorleap.ai>
2025-10-22 14:37:20 +0300

    ignored test

--- test/models/test_llama_sentiment_analysis.py
+++ test/models/test_llama_sentiment_analysis.py
@@ -11,7 +11,7 @@ from onnx2kerastl.customonnxlayer import onnx_custom_objects_map
 from test.utils import export_torch_to_onnx_optimum
 
 
-@pytest.mark.skip(reason="Fails on CI but works locally (might be too big?)")
+# @pytest.mark.skip(reason="Fails on CI but works locally (might be too big?)")
 def test_llama_32_1b_inst():
     onnx_model_folder = 'onnx_model'
     onnx_path = os.path.join(onnx_model_folder, 'model.onnx')
@@ -53,5 +53,51 @@ def test_llama_32_1b_inst():
 
     assert pred_token=='positive'
 
+
+# @pytest.mark.skip(reason="Fails on CI but works locally (might be too big?)")
+def test_llama_32_1b():
+    onnx_model_folder = 'onnx_model'
+    onnx_path = os.path.join(onnx_model_folder, 'model.onnx')
+    model_name = "meta-llama/Llama-3.2-1B"
+    # --------------------------------- Export to ONNX -------------------------------------
+    export_torch_to_onnx_optimum(model_name, model_output_path=onnx_model_folder)
+    # ----------------------------------------- Input Preparation --------------------------
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+    text = "I love this movie!"
+
+    # Construct a direct prompt for regular LLMs
+    prompt_text = f"What is the sentiment of this sentence: \"{text}\"? Respond with 'positive' or 'negative' only. The sentiment is"
+
+    # Tokenize normally (no chat template)
+    encoding = tokenizer(prompt_text, return_tensors="pt", padding='max_length', truncation=True, max_length=128)
+
+    input_ids = encoding["input_ids"]
+    attention_mask = encoding["attention_mask"]
+    position_ids = np.arange(input_ids.shape[1])[None, :]
+
+    model_inputs = {
+        "input_ids": input_ids,
+        "attention_mask": attention_mask,
+        "position_ids": position_ids
+    }
+    keras_inputs = {k: tf.convert_to_tensor(v) for k, v in model_inputs.items()}
+    # --------------------------------- Export to Keras -------------------------------------
+    onnx_model = onnx.load(onnx_path)  # TODO: add to requirements, updated onnx==1.17.0 ()
+    keras_model = onnx_to_keras(onnx_model, ['input_ids', 'attention_mask', 'position_ids'],
+                                allow_partial_compilation=False)
+    keras_model = keras_model.converted_model
+    flipped_model = convert_channels_first_to_last(keras_model, [])
+    flipped_model.save('temp.h5')
+    model = tf.keras.models.load_model('temp.h5', custom_objects=onnx_custom_objects_map)
+    # --------------------------------- Evaluating Inference -------------------------------------
+    outputs = model(keras_inputs)
+    # last_token_logits = outputs[0, -1]
+    # pred_token_id = np.argmax(last_token_logits)
+    # pred_token = tokenizer.decode([pred_token_id]).strip().lower()
+    pred_token = tokenizer.decode(np.argmax(outputs, axis=2)[0], skip_special_tokens=True).split()[-1]
+
+    assert pred_token=='positive'
+
 if __name__ == "__main__":
-    test_llama_32_1b_inst()
+    test_llama_32_1b()
