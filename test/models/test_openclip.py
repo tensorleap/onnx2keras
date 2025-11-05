1a3c07b531caf7d42d49a1e497966898ed23e149
Idan <46688952+tip3x@users.noreply.github.com>
2025-11-05 09:07:51 +0200

    Bugfix | gather with minus in indices (#200)
    * .
    
    * .
    
    * >
    
    * >
    
    * >
    
    ---------
    
    Co-authored-by: Idan <idan@idan.com>

--- test/models/test_openclip.py
+++ test/models/test_openclip.py
@@ -6,16 +6,14 @@ from keras_data_format_converter import convert_channels_first_to_last
 import urllib
 
 
-def test_raft_ci():
+def test_openclip():
     urllib.request.urlretrieve(
-        "https://storage.googleapis.com/example-datasets-47ml982d/raft/raft.onnx",
-        "raft.onnx")
-    onnx_model = onnx.load('raft.onnx')
-    keras_model = onnx_to_keras(onnx_model, ['onnx::Div_0', 'onnx::Div_1'], name_policy='attach_weights_name',
+        "https://storage.googleapis.com/example-datasets-47ml982d/openclip/openclip.onnx",
+        "openclip.onnx")
+    onnx_model = onnx.load('openclip.onnx')
+    keras_model = onnx_to_keras(onnx_model, ["pixel_values"], name_policy='attach_weights_name',
                                 allow_partial_compilation=False)
     keras_model = keras_model.converted_model
     final_model = convert_channels_first_to_last(keras_model, should_transform_inputs_and_outputs=True)
-    first_im = np.random.random((440, 1024, 3))[None, ...]
-    second_im = np.random.random((440, 1024, 3))[None, ...]
-    tf_preds = final_model([first_im, second_im])
+    tf_preds = final_model(np.random.random((1, 224, 224, 3)))
 
