7e43f12ce79578d69190f3e68b8045a4373d80ce
ranhomri <ran.homri@tensorleap.ai>
2026-02-25 14:19:58 +0200

    aws utils with simpler local testing

--- test/models/private_tests/aws_utils.py
+++ test/models/private_tests/aws_utils.py
@@ -7,13 +7,8 @@ import tempfile
 BUCKET_NAME = 'tensorleap-engine-tests-dev'
 PREFIX = 'onnx2keras'
 if not 'LOCAL_TEST' in os.environ:
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        aws_session_token=os.environ['AWS_SESSION_TOKEN'],
-        region_name='us-east-1'
-    )
+    session = boto3.Session(region_name='us-east-1')
+    s3 = session.client('s3')
 
 @pytest.fixture
 def aws_s3_download(request):
