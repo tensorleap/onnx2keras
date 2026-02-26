b24d46b807850637ef88c12999428977eb8d3a98
ranhomri <ran.homri@tensorleap.ai>
2026-02-26 11:01:18 +0200

    revert aws_utils to env-var based auth for CI

--- test/models/private_tests/aws_utils.py
+++ test/models/private_tests/aws_utils.py
@@ -7,8 +7,13 @@ import tempfile
 BUCKET_NAME = 'tensorleap-engine-tests-dev'
 PREFIX = 'onnx2keras'
 if not 'LOCAL_TEST' in os.environ:
-    session = boto3.Session(profile_name='dev')
-    s3 = session.client('s3', region_name='us-east-1')
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+        aws_session_token=os.environ['AWS_SESSION_TOKEN'],
+        region_name='us-east-1'
+    )
 
 @pytest.fixture
 def aws_s3_download(request):
