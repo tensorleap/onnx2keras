64204674900adaa62753c689d5331bb4ebc863c8
ranhomri <ran.homri@tensorleap.ai>
2026-03-02 11:12:43 +0200

    Add multi-axes parametrization to _input variant tests to prevent false negatives from fallback defaults

--- test/layers/operations/test_reduce_sum_opset.py
+++ test/layers/operations/test_reduce_sum_opset.py
@@ -4,9 +4,11 @@ from onnx import helper, TensorProto, numpy_helper
 
 from test.layers.onnx_op_test_utils import make_single_op_model, run_op_test
 
+_INPUT_SHAPE = [2, 3, 4]
+
 
 def _make_reduce_sum_model_attr(axes, keepdims, opset_version):
-    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, [2, 3, 4])
+    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, _INPUT_SHAPE)
     out_info = helper.make_tensor_value_info('reduced', TensorProto.FLOAT, [2, 1, 4])
     node = helper.make_node('ReduceSum', inputs=['data'], outputs=['reduced'],
                             axes=axes, keepdims=keepdims)
@@ -14,8 +16,9 @@ def _make_reduce_sum_model_attr(axes, keepdims, opset_version):
 
 
 def _make_reduce_sum_model_input(axes, keepdims, opset_version):
-    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, [2, 3, 4])
-    out_info = helper.make_tensor_value_info('reduced', TensorProto.FLOAT, [2, 1, 4])
+    out_shape = [s if i not in axes else 1 for i, s in enumerate(_INPUT_SHAPE)]
+    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, _INPUT_SHAPE)
+    out_info = helper.make_tensor_value_info('reduced', TensorProto.FLOAT, out_shape)
     axes_init = numpy_helper.from_array(np.array(axes, dtype=np.int64), name='axes')
     node = helper.make_node('ReduceSum', inputs=['data', 'axes'], outputs=['reduced'],
                             keepdims=keepdims)
@@ -30,8 +33,9 @@ def test_reduce_sum_axes_attr(opset_version):
     run_op_test(model, {'data': data}, ['data'])
 
 
+@pytest.mark.parametrize('axes', ([0], [2]))
 @pytest.mark.parametrize('opset_version', [13])
-def test_reduce_sum_axes_input(opset_version):
+def test_reduce_sum_axes_input(axes, opset_version):
     data = np.random.randn(2, 3, 4).astype(np.float32)
-    model = _make_reduce_sum_model_input([1], 1, opset_version)
+    model = _make_reduce_sum_model_input(axes, 1, opset_version)
     run_op_test(model, {'data': data}, ['data'])
--- test/layers/reshapes/test_unsqueeze_opset.py
+++ test/layers/reshapes/test_unsqueeze_opset.py
@@ -5,16 +5,27 @@ from onnx import helper, TensorProto, numpy_helper
 from test.layers.onnx_op_test_utils import make_single_op_model, run_op_test
 
 
+def _insert_dims(shape, axes):
+    result = list(shape)
+    for ax in sorted(axes):
+        result.insert(ax, 1)
+    return result
+
+
 def _make_unsqueeze_model_attr(axes, opset_version):
-    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, [3, 4])
-    out_info = helper.make_tensor_value_info('unsqueezed', TensorProto.FLOAT, [1, 3, 4])
+    in_shape = [3, 4]
+    out_shape = _insert_dims(in_shape, axes)
+    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, in_shape)
+    out_info = helper.make_tensor_value_info('unsqueezed', TensorProto.FLOAT, out_shape)
     node = helper.make_node('Unsqueeze', inputs=['data'], outputs=['unsqueezed'], axes=axes)
     return make_single_op_model('Unsqueeze', [node], [data_info], [out_info], opset_version)
 
 
 def _make_unsqueeze_model_input(axes, opset_version):
-    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, [3, 4])
-    out_info = helper.make_tensor_value_info('unsqueezed', TensorProto.FLOAT, [1, 3, 4])
+    in_shape = [3, 4]
+    out_shape = _insert_dims(in_shape, axes)
+    data_info = helper.make_tensor_value_info('data', TensorProto.FLOAT, in_shape)
+    out_info = helper.make_tensor_value_info('unsqueezed', TensorProto.FLOAT, out_shape)
     axes_init = numpy_helper.from_array(np.array(axes, dtype=np.int64), name='axes')
     node = helper.make_node('Unsqueeze', inputs=['data', 'axes'], outputs=['unsqueezed'])
     return make_single_op_model('Unsqueeze', [node], [data_info], [out_info], opset_version,
@@ -28,8 +39,9 @@ def test_unsqueeze_axes_as_attribute(opset_version):
     run_op_test(model, {'data': data}, ['data'])
 
 
+@pytest.mark.parametrize('axes', ([0], [2]))
 @pytest.mark.parametrize('opset_version', [13])
-def test_unsqueeze_axes_as_input(opset_version):
+def test_unsqueeze_axes_as_input(axes, opset_version):
     data = np.random.randn(3, 4).astype(np.float32)
-    model = _make_unsqueeze_model_input([0], opset_version)
+    model = _make_unsqueeze_model_input(axes, opset_version)
     run_op_test(model, {'data': data}, ['data'])
